3ebc7c6db915efe38d77a9988fb7543943bdb48b
Chazarow <zaharowanrew@gmail.com>
2026-02-28 00:04:19 +0400

    fix: config, docker-compose, dockerfile

--- labs/1-development-of-a-multilayer-perceptron-neuroemulator/resolve/simple_emulator/config.py
+++ labs/1-development-of-a-multilayer-perceptron-neuroemulator/resolve/simple_emulator/config.py
@@ -4,10 +4,10 @@ load_dotenv()
 
 
 class Config:
-    JWT_SEKRET = os.getenv("JWT_SEKRET", "default-secret-change-me")
-    JWT_EXPIRES_AT = int(os.getenv("JWT_EXPIRES_AT", "3600"))
+    JWT_SEKRET = os.getenv("JWT_SEKRET")
+    JWT_EXPIRES_AT = 3600
 
-    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/perceptron_db")
+    DATABASE_URL = os.getenv("DATABASE_URL")
 
     IMAGES_DIRECTORY = "data/visualisation"
     CSV_DIRECTORY = "data/learn"
